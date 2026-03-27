50561cdfb549d2bdd87a70d30c6ff2345738a524
llacuna750 <llacuna750@gmail.com>
2026-03-28 01:21:56 +0800

    Add Order Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,4 +21,9 @@ class User extends Model
     // protected $primaryKey = userid;
 
     public $timestamps = false;
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'user_id', 'id');
+    }
 }
